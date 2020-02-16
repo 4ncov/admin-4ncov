d9796a29c959a8f2998a9ee6847984c386aba880
ran.meng <lrcry@vip.qq.com>
2020-02-16 17:31:12 +0800

    feat: listing and authorization

--- src/api/required-materials.js
+++ src/api/required-materials.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function list(page, size, userId, status) {
   return request({
-    url: `/required-materials?userId=${userId || ''}&status=${status || ''}&page=${page}&size=${size}`,
+    url: `/required-materials/all?userId=${userId || ''}&status=${status || ''}&page=${page}&size=${size}`,
     method: 'get'
   })
 }
--- src/api/supplied-materials.js
+++ src/api/supplied-materials.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function list(page, size, userId, status) {
   return request({
-    url: `/supplied-materials?userId=${userId}&status=${status}&page=${page}&size=${size}`,
+    url: `/supplied-materials/all?userId=${userId}&status=${status}&page=${page}&size=${size}`,
     method: 'get'
   })
 }
